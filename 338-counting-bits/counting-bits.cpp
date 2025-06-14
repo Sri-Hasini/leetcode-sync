7ffde08caf5ee02ced5bc8eda86b0af0c544885c
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-06-14 15:20:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (80.35%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,19 +1,11 @@
+#include <vector>
+using namespace std;
 class Solution {
 public:
-    int helper(int n) {
-        int cnt = 0;
-        while (n > 0) {
-            if (n & 1) {
-                cnt++;
-            }
-            n /= 2;
-        }
-        return cnt;
-    }
     vector<int> countBits(int n) {
-        vector <int> ans;
-        for (int i = 0; i <= n; i++) {
-            ans.push_back(helper(i));
+        vector<int> ans(n + 1);
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
         return ans;
     }
