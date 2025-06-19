feccbfc332635401f3a1895be14b64a4ce870a2b
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-06-19 22:23:57 +0530

    Time: 6 ms (19.30%) | Memory: 32.1 MB (61.95%) - LeetSync

--- 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
+        // think in reverse way, instead of doing for n - 1 elements increment... do for 1 element subtracting... so it would be easier to apply our logic
         sort(nums.begin(), nums.end());
         int x = nums[0];
         int ans = 0;
