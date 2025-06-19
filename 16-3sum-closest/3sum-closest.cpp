7d4fecfae91f7b86ffcd371771242e1f5ef5a276
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-06-19 21:43:04 +0530

    Time: 14 ms (41.61%) | Memory: 14.1 MB (27.81%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -5,18 +5,14 @@ public:
         int closest = nums[0] + nums[1] + nums[2]; // initialize to first triplet
         
         for (int i = 0; i < nums.size() - 2; i++) {
-            int left = i + 1;
-            int right = nums.size() - 1;
+            int left = i + 1, right = nums.size() - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (abs(target - sum) < abs(target - closest)) {
+                if (abs(target - closest) > abs(target - sum)) {
                     closest = sum;
                 }
-                if (sum < target) {
-                    left++;
-                } else {
-                    right--;
-                }
+                if (sum < target) left++;
+                else right--;
             }
         }
         return closest;
