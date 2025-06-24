c7d868c71cff2b533780da99945c72256342366c
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-06-24 19:30:07 +0530

    Time: 70 ms (30.72%) | Memory: 218.7 MB (20.26%) - LeetSync

--- 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
+++ 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     long long matrixSumQueries(int n, vector<vector<int>>& q) {
-        reverse(q.begin(), q.end());
-        unordered_set<int> s1, s2;
-        int r = 0, c = 0;
-        long long ans = 0;
-
-        for (auto &i : q) {
-            int t = i[0], ind = i[1], val = i[2];
+        reverse(q.begin(), q.end()); // perform this question from reverse 
+        long long ans = 0LL;
+        unordered_set <int> s1, s2;// for storing already visited rows and cols
+        int r = 0, c = 0;// for counting already visited rows and cols
+        for (int i = 0; i < q.size(); i++){
+            int t = q[i][0], ind = q[i][1], val = q[i][2];
             if (t == 0) {
                 if (s1.find(ind) == s1.end()) {
                     ans += 1LL * val * (n - c);
-                    s1.insert(ind);
                     r++;
+                    s1.insert(ind);
                 }
-            } else {
+            }
+            else {
                 if (s2.find(ind) == s2.end()) {
                     ans += 1LL * val * (n - r);
-                    s2.insert(ind);
                     c++;
+                    s2.insert(ind);
                 }
             }
         }
