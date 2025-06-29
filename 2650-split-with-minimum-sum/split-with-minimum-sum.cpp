521c97852467570446af51dc5ce365092b20e5f5
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-06-29 17:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (82.39%) - LeetSync

--- 2650-split-with-minimum-sum/split-with-minimum-sum.cpp
+++ 2650-split-with-minimum-sum/split-with-minimum-sum.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     int splitNum(int num) {
         vector<int> digits;
-        
-        // Extract digits
         while (num > 0) {
             digits.push_back(num % 10);
             num /= 10;
         }
-        // Sort digits in ascending order
         sort(digits.begin(), digits.end());
         int num1 = 0, num2 = 0;
-
-        // Distribute digits alternately
         for (int i = 0; i < digits.size(); ++i) {
             if (i % 2 == 0) {
                 num1 = num1 * 10 + digits[i];
@@ -20,7 +15,6 @@ public:
                 num2 = num2 * 10 + digits[i];
             }
         }
-        // Return the minimal possible sum
         return num1 + num2;
     }
 };
