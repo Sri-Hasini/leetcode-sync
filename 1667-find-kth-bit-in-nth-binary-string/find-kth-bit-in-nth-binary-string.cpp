ffeba0276fdb9b8b92f57e9c768ddb749afb6f5d
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-07-01 22:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.01%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-    string invert(string h) {
-        string ans;
-        for (int i = 0; i < h.size(); i++) {
-            if (h[i] == '0') ans += '1';
-            else ans += '0';
-        }
-        return ans;
-    }
-    string rev(string h) {
-        reverse(h.begin(), h.end());
-        return h;
-    }
     char findKthBit(int n, int k) {
-        string pre = "0", cur = "";
-        n--;
-        while (n--) {
-            cur += pre;
-            cur += "1";
-            string k = invert(pre);
-            cur += rev(k);
-            pre = cur;
-            cur = "";
-        }
-        return pre[k - 1];
+        
+        if( n == 1 ) return '0';
+        int len = (1 << n) - 1;
+        int mid = len / 2 + 1;
+        if( k == mid ) return '1';
+        if( k < mid ) return findKthBit(n-1, k);
+        return findKthBit(n-1, len - k + 1) == '0' ? '1' : '0';
     }
 };
