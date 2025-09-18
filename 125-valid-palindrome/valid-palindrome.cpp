559620ebabd0c94aa2cfca403df1bc9077776438
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-09-18 20:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (35.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string h = "";
-        for (auto i : s) {
-            if ((i >= 'A' && i <= 'Z') || (i >= 'a' && i <= 'z') || (i >= '0' && i <= '9')) {
-                if (i >= 'A' && i <= 'Z') h += (i + 32);
-                else h += i;
-            }
+        string h;
+        int n=s.size();
+        // cout<<n<<endl<<s;
+        char *i=&s[0];
+        char *j=&s[0]+n-1;
+        char z;
+        // cout<<n<<" "<<*i<<" "<<*j<<endl;
+        if(n==1){
+            return true;
         }
-        int left = 0, right = h.size() - 1;
-        while (left <= right) {
-            if (h[left] == h[right]) {
-                left++;
-                right--;
+        else{
+            while(i<=j){
+                // cout<<*i<<endl;
+                if((*i>='A' && *i<='Z')||(*i>='a' && *i<='z')||(*i>='0' && *i<='9')){
+                    if(*i>='A' && *i<='Z'){
+                        z=*i+32;
+                        // cout<<*i<<" "<<z<<endl;
+                        h+=z;
+                        i++;
+                        continue;
+                    }
+                    h+=*i;
+                }
+                
+                i++;
             }
-            else {
-                return false;
+            int n1=h.size();
+            if(n1<=1) return true;
+            char *k=&h[0];
+            char *q=&h[0]+n1-1;
+            bool g=false;
+            while(k<q){
+                if(*k==*q){
+                    k++;
+                    q--;
+                    g=true;
+                    continue;
+                }
+                else{
+                    g=false;
+                    break;
+                }
             }
+            return g;
         }
-        return true;
     }
 };
