44c656b6c3e161363d2e979d1aa67427e8b37c66
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-09-28 21:56:31 +0530

    Time: 3 ms (31.26%) | Memory: 17 MB (6.05%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void subsequence(vector <int> v,vector <int> ans, vector<vector<int>> &ds, int n) {
+        if(n < 0) {
+            ds.push_back(ans);
+            return;
+        }
+        ans.push_back(v[n]);
+        subsequence(v,ans,ds,n-1);
+        ans.pop_back();
+        subsequence(v,ans,ds,n-1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size()-1;
+        vector<vector<int>> ds ;
+        vector <int> ans;
+        subsequence(nums,ans,ds,n);
+        return ds;
+    }
+};
