b53728b9a04705ac74c2b81e09ded43fba23dd06
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-10-17 23:21:41 +0530

    Time: 12 ms (9.94%) | Memory: 11.7 MB (60.25%) - LeetSync

---
+++ 2791-find-the-losers-of-the-circular-game/find-the-losers-of-the-circular-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) {
+        vector <int> ans;
+        int cnt = 1, cur = 1;
+        vector <int> a(n + 1);
+        a[1] = 1;
+        while (a[cur] != 2) {
+            a[(cur + (cnt * k)) % n]++;
+            cur = (cur + (cnt * k)) % n;
+            cout << cur;
+            cnt++;
+        }
+        a[a.size() - 1] = a[0];
+        for (int i = 1; i < a.size(); i++) {
+            if (a[i] == 0) ans.push_back(i);
+        }
+        return ans;
+    }
+};
