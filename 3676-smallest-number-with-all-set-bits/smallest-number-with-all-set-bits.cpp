ef4bfc54eec093aeec291e69adf53f0eaf3b5e59
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-10-29 15:20:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.95%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int x;
-        for(int i = 0; i <= 10 ; i++) {
-            if(pow(2,i) > n) {
-                x = i;
-                break;
-            }
-        }
-        return pow(2,x)-1;
+        int x = log2(n) + 1;
+        x = (1 << x) - 1;
+        return x;
     }
 };
