988b196bd8573ca8f2567486834908ba64546577
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-11-16 22:07:13 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (33.33%) - LeetSync

---
+++ 4090-minimum-string-length-after-balanced-removals/minimum-string-length-after-balanced-removals.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(string s) {
+        int a = 0, b = 0;
+        for (auto i : s) {
+            if (i == 'a') a++;
+            else b++;
+        }
+        return abs(a - b);
+    }
+};
